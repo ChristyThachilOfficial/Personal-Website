f6aac4faeb809abc3b7c6ef46c5146fc8dfc7f3b
Christy Thachil <christythachil000@gmail.com>
2021-08-07 09:33:30 +0530

    edited

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-$(document).ready(function(){
-    $('#submit-form').validate({
-        rules:{
-            fullname:{
-                required:true,
-                minlength:2,
-                digits:false
-                
 
-            },
-            Email:{
+$(document).ready(function(){
+    $('#signup-form').validate({
+        
+        rules:{
+            fname:{
                 required:true,
-                email:true
+                minlength:3
+                
             },
-            subject:{
-                required:true
+            lname:{
+                required:true,
+                minlength:3
+                
+            },
+            email:{
+                required:true,
+                minlength:10
+                
+                
+            },
+            message:{
+                required:true,
+                minlength:10
+                
+            },
+            mobile:{
+                required:true,
+                number:true,
+                minlength:10,
+                maxlength:10
             }
         },
+        messages:{
+            email:"enter valid email",
+            message:"Type something"
+            
+        },
+            
+        
         submitHandler :function(form){
             $.ajax({
                 url:"https://script.google.com/macros/s/AKfycbwAgmUw3yAyfqFen8Seh9fT-CaR0CnAieeXaZ81G_oLGH_emBKCM3YuhQynrB02A7Uk/exec",
-                data:$("#submit-form").serialize(),
+                data:$("#signup-form").serialize(),
                 method:"post",
                 success:function (response){
                     alert("Form submitted successfully")
